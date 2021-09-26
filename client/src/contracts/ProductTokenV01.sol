9ea6afd752bbbfb647db95ae32d8fb5baa8605fc
JackShiu <poming.shiu@gmail.com>
2021-09-26 14:13:14 +0800

    add ProductTokenV01

--- client/src/contracts/ProductTokenV01.sol
+++ client/src/contracts/ProductTokenV01.sol
@@ -18,13 +18,67 @@ contract ProductTokenV0 is ProductToken {
         uint256 amount;
         address wallet;
     }
+    struct UserInfo {
+        uint256 amount;
+        uint256 rewardDebt;
+        uint256 index;
+        uint256[] records;
+    }
+    struct PoolInfo {
+        uint256 amount; // record the entire pool vaule
+        uint256 accRewardPerShare; // Accumulated reward per share
+        uint256 tokenReward; //total reward
+    }
     struct voucherInfo {
         address addr;
         uint256 tokenId;
     }
 
     supplierInfo public supplier;
+    PoolInfo public poolInfo;
     voucherInfo public voucher;
+    mapping (address => UserInfo) public userInfo;
+
+    function _updatePool(uint256 fee_) internal virtual{
+        uint256 supply = poolInfo.amount;
+        if (supply == 0) {
+            poolInfo.accRewardPerShare = 0;
+            return;
+        }
+        poolInfo.accRewardPerShare = poolInfo.accRewardPerShare.add(fee_.mul(1e12).div(supply));
+    }
+
+    function _updateSellStaking(uint32 amount_, uint256 tokenId_) internal virtual {
+        UserInfo storage user = userInfo[msg.sender];
+        require(user.amount > 0, "invalid amount");
+        if(balanceOf(msg.sender) <= user.records.length) {
+            uint max = uint256(amount_);
+            if(max > user.records.length) {
+                max = user.records.length;
+            }
+            uint256 value = 0;
+            for(uint i = 0; i < max; i++) {
+                value = value.add(user.records[user.records.length.sub(1)]);
+                user.records.pop();
+            }
+            PoolInfo storage pool = poolInfo;
+            uint256 reward = user.amount.mul(pool.accRewardPerShare).div(1e12).sub(user.rewardDebt);
+            poolInfo.amount = poolInfo.amount.sub(value);
+            if(reward > 0) {
+                IVNFT(voucher.addr).transferFrom(address(this), msg.sender, voucher.tokenId, tokenId_, reward);
+                poolInfo.tokenReward = poolInfo.tokenReward.sub(reward);
+            }
+            user.amount = user.amount.sub(value);
+            user.rewardDebt = user.amount.mul(poolInfo.accRewardPerShare).div(1e12);
+        }
+    }
+
+    function _updateUserInfo(uint256 price_ ) internal virtual {
+        UserInfo storage user = userInfo[msg.sender];
+        user.amount = user.amount.add(price_);
+        user.records.push(price_);
+        user.rewardDebt = user.amount.mul(poolInfo.accRewardPerShare).div(1e12);
+    }
 
     function setupVoucher(address addr_, uint256 tokenId_) external virtual onlyOwner{
         require(addr_ != address(0), 'invalid address');
@@ -51,6 +105,18 @@ contract ProductTokenV0 is ProductToken {
                 instance.transferFrom(address(this), msg.sender, voucher.tokenId, tokenId_, change);
             }
             _updateSupplierFee(fee.mul(1e12).div(8e12));
+            uint256 reward = fee.mul(6e12).div(8e12);
+            _updatePool(reward);
+            poolInfo.tokenReward = poolInfo.tokenReward.add(reward);
+            UserInfo storage user = userInfo[msg.sender];
+            PoolInfo storage pool = poolInfo;
+            uint256 pending = user.amount.mul(pool.accRewardPerShare).div(1e12).sub(user.rewardDebt);
+            if(pending > 0) {
+                instance.transferFrom(address(this), msg.sender, voucher.tokenId, tokenId_, pending);
+                poolInfo.tokenReward = poolInfo.tokenReward.sub(pending);
+            }
+            poolInfo.amount = poolInfo.amount.add(price);
+            _updateUserInfo(price);
         } else {
             instance.transferFrom(address(this), msg.sender, voucher.tokenId, tokenId_, maxPrice_);
         }
@@ -58,6 +124,9 @@ contract ProductTokenV0 is ProductToken {
 
     function sellByVoucher(uint256 tokenId_, uint32 amount_) external virtual onlyIfTradable{
         (uint256 price, uint256 fee )= _sellForAmount(amount_);
+
+        _updateSellStaking(amount_, tokenId_);
+
         IVNFT(voucher.addr).transferFrom(address(this), msg.sender, voucher.tokenId, tokenId_, price);
         _updateSupplierFee(fee.mul(1e12).div(2e12));
     }
@@ -73,6 +142,9 @@ contract ProductTokenV0 is ProductToken {
         _burn(msg.sender, amount_);
         tradeinCount = tradeinCount + amount_;
         tradeinReserveBalance = tradeinReserveBalance.add(tradinReturn);
+
+        _updateSellStaking(amount_, tokenId_);
+
         emit Tradein(msg.sender, amount_);
     }
 
@@ -103,4 +175,12 @@ contract ProductTokenV0 is ProductToken {
         return supplier.amount;
     }
 
+    // function getUserReward(address addr_) external view virtual returns (uint256) {
+    //     if(userInfo[addr_].amount > 0) {
+    //         UserInfo storage user = userInfo[addr_];
+    //         PoolInfo storage pool = poolInfo;
+    //         return user.amount.mul(pool.accRewardPerShare).div(1e12).sub(user.rewardDebt);
+    //     }
+    //     return 0;
+    // }
 }
